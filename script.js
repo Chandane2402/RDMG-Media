4ca6e966654ac0f3fc7f8794a08ca3ae8bf07255
Chandane2402 <chandanrace0204@gmail.com>
2025-02-18 23:19:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mobile Menu Toggle
+function toggleMenu() {
+    document.getElementById("side-menu").classList.toggle("show");
+}
+
+// Kannada Language Toggle
+document.getElementById("toggleKannada").addEventListener("click", function() {
+    alert("Kannada language toggle coming soon!");
+});
